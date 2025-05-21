831cf1d32cd1b1cac34dc582f09e18deba4c946d
Aahadalkhaldi <admin@aljasssim.com>
2025-05-21 07:13:20 +0300

    Add files via upload

---
+++ firebase-config (1).js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVXtDHaS3j4FeZsXllgcWu5o4Fg8FdJe4",
+  authDomain: "legalplatform-a43f8.firebaseapp.com",
+  projectId: "legalplatform-a43f8",
+  storageBucket: "legalplatform-a43f8.appspot.com",
+  messagingSenderId: "558959612033",
+  appId: "1:558959612033:web:bb341b0477fd675f6b7d80"
+};
