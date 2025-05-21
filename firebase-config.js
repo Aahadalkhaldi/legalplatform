334f68cad6b21188aa1ef14fcbd2889af8f55fba
Aahadalkhaldi <admin@aljasssim.com>
2025-05-21 08:38:55 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBVXtDHaS3j4FeZsXllgcWu5o4Fg8FdJe4",
   authDomain: "legalplatform-a43f8.firebaseapp.com",
   projectId: "legalplatform-a43f8",
-  storageBucket: "legalplatform-a43f8.firebasestorage.app",
+  storageBucket: "legalplatform-a43f8.appspot.com",
   messagingSenderId: "558959612033",
   appId: "1:558959612033:web:bb341b0477fd675f6b7d80"
 };
